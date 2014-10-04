bea18b3a61ca3af1db7934d65ffe7a3af1b71b35
Paul Nickerson <pvnick@gmail.com>
2014-10-04 16:17:04 -0400

    mpi now establishes leaders among same-machine procs

--- Source Code/main.cpp
+++ Source Code/main.cpp
@@ -4,6 +4,7 @@
 #include "find_motifs.h"
 #include <iostream>
 #include <string>
+#include <pair>
 
 #ifdef USE_PROFILER
     #include "profiler.h"
@@ -11,10 +12,91 @@
 #ifdef USE_MPI
     #include <boost/mpi.hpp>
     #include <boost/serialization/string.hpp>
+    #include <boost/asio.hpp>
 
+    typedef unsigned int rank_id
     namespace mpi = boost::mpi;
+    typedef std::pair<std::string, rank_id> hostname_rank_pair;
+    static rank_id root_rank = 0;
+    static std::map<std::string, rank_id> hostname_leaders;
+    static bool hostname_leaders_elected = false;
 
-    size_t query_start_position_by_params(size_t series_length, unsigned int proc_rank, unsigned int num_procs) {
+    std::ostream& msg(std::string str) {
+        mpi::communicator world;
+        std::cout << "Proc " << world.rank() << ": " << str;
+        return std::cout;
+    }
+
+    std::string hostname() {
+        return boost::asio::ip::host_name();
+    }
+
+    bool is_root() {
+        mpi::communicator world;
+        return world.rank() == root_rank;
+    }
+
+    void elect_hostname_leaders() {
+        mpi::communicator world;
+        if ( ! hostname_leaders_elected) {
+            hostname_rank_pair my_hostname_rank(hostname(), world.rank());
+            if (is_root()) {
+                std::vector<hostname_rank_pair> all_hostname_ranks;
+                //root gathers hostname-rank pairs from all procs, elects the hostname leaders,
+                //then broadcasts the leader info out to all procs
+                mpi::gather(world, my_hostname_rank, all_hostname_ranks, root_rank);
+                for (auto hostname_rank: all_hostname_ranks) {
+                    std::string hn = hostname_rank.first;
+                    rank_id r = hostname_rank.second;
+                    if (hostname_leaders.find(hn) == hostname_leaders.end())
+                        hostname_leaders[hn] = r;
+                    else
+                        hostname_leaders[hn] = std::min(hostname_leaders[hn], r); //leader rank is minimum rank for that hostname
+                }
+                mpi::broadcast(world, hostname_leaders, root_rank);
+            } else {
+                //this proc is not the root, so send the hostname-rank pair to the root, then wait
+                //for the root to broadcast the leaders
+                mpi::gather(world, my_hostname_rank, root_rank);
+                mpi::broadcast(world, hostname_leaders, root_rank);
+            }
+            hostname_leaders_elected = true;
+        }
+    }
+
+    rank_id get_hostname_leader() {
+        elect_hostname_leaders();
+        rank_id hostname_leader = hostname_leaders[hostname()];
+        msg("Leader among ") << hostname() << " is " << hostname_leader << std::endl;
+        return hostname_leader;
+    }
+
+    bool is_hostname_leader() {
+        mpi::communicator world;
+        return world.rank() == get_hostname_leader();
+    }
+
+    MotifFinder get_engine() {
+        //engine cache takes up a few gigs of memory. therefore, only initialize it once per machine,
+        //then share the memory among all other procs on the same machine
+        mpi::environment env;
+        mpi::communicator world;
+        rank_id my_rank = world.rank();
+        MotifFinder engine;
+        if (is_hostname_leader()) {
+            //current process is the leader on this machine. tell the motif finder engine to allocate the cache, then share its memory
+            engine->init();
+            mpi::broadcast(world, shared memory, hostname_leader);
+        } else {
+            //a different process is the leader on this machine. wait for it to allocate the cache then tell
+            //the motif finder engine to use its shared memory
+            mpi::broadcast(world, shared memory, hostname_leader);
+        }
+
+        return engine;
+    }
+
+    size_t query_start_position_by_params(size_t series_length, rank_id proc_rank, unsigned int num_procs) {
         proc_rank = num_procs - proc_rank; //make assignment easier to conceptualize
         size_t search_space_length = floor((float)proc_rank / num_procs * series_length * (series_length + 1.0) / 2.0);
         size_t position = ceil(-1.0 / 2.0 * sqrt(8.0 * search_space_length + 1.0) + series_length + 1.0 / 2.0);
@@ -25,16 +107,16 @@
         mpi::environment env;
         mpi::communicator world;
         unsigned int num_procs = world.size();
-        unsigned int proc_rank = world.rank();
-        return query_start_position_by_params(TIME_SERIES_LEN, proc_rank, num_procs);
+        rank_id my_rank = world.rank();
+        return query_start_position_by_params(TIME_SERIES_LEN, my_rank, num_procs);
     }
 
     size_t my_query_end_pos() {
         mpi::environment env;
         mpi::communicator world;
         unsigned int num_procs = world.size();
-        unsigned int proc_rank = world.rank();
-        return query_start_position_by_params(TIME_SERIES_LEN, proc_rank + 1, num_procs);
+        rank_id my_rank = world.rank();
+        return query_start_position_by_params(TIME_SERIES_LEN, my_rank + 1, num_procs);
     }
 #else
     size_t my_query_start_pos() {
@@ -44,10 +126,16 @@
     size_t my_query_end_pos() {
         return TIME_SERIES_LEN;
     }
+
+    MotifFinder get_engine() {
+        MotifFinder engine;
+        engine->init();
+        return engine;
+    }
 #endif
 
 /// Main Function
-int main(  int argc , char *argv[] )
+int main(int argc , char *argv[] )
 {
     /*
     int num_procs = 100;
@@ -61,7 +149,7 @@ int main(  int argc , char *argv[] )
     unsigned int K = 100;
     size_t start_pos = my_query_start_pos();
     size_t end_pos = my_query_end_pos();
-    MotifFinder engine;
+    MotifFinder engine = get_engine();
     #ifdef USE_PROFILER
         ProfilerStart("/tmp/profile");
         end_pos = start_pos + 3;
